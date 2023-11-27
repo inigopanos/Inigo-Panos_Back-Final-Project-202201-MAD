97d7cca7aa213f058a39e11199c0cc24a3060a2b
Iñigo <inigopanos@gmail.com>
2023-11-27 17:02:21 +0100

    Cambiado db.js

--- services/db.js
+++ services/db.js
@@ -4,15 +4,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export async function mongoConnect() {
-    const userName = process.env.DBUSER;
-    const password = process.env.DBPASSWD;
+   
     let dbName;
     if (process.env.NODE_ENV === 'test') {
         dbName = process.env.DBNAMETEST;
     } else {
         dbName = process.env.DBNAME;
     }
-
+    const userName = process.env.DBUSER;
+    const password = process.env.DBPASSWD;
+    
     const uri = `mongodb+srv://${userName}:${password}@cluster0.piemq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     // const uri = `mongodb+srv://inigopanos:SmkJEOJot1X5Mbrc@cluster0.piemq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     console.log('Este es el uri:', uri);
