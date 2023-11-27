6a88aea95d8c3eac1a55fd7f559e60e53d5ed54f
Iñigo <inigopanos@gmail.com>
2023-11-27 16:55:33 +0100

    Quitado username añadido nombre a pelo

--- services/db.js
+++ services/db.js
@@ -14,7 +14,7 @@ export async function mongoConnect() {
     }
 
     // const uri = `mongodb+srv://${userName}:${password}@cluster0.piemq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-    const uri = `mongodb+srv://${userName}:SmkJEOJot1X5Mbrc@cluster0.piemq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://inigopanos:SmkJEOJot1X5Mbrc@cluster0.piemq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     const mongooseConnect = await mongoose.connect(uri);
     return mongooseConnect;
 }
